6b1f3804bd3c628de7ad35dbba2c49470a335ebd
Sushant3559 <168175123+Sushant3559@users.noreply.github.com>
2026-09-01 20:10:52 +0530

    Time: 1 ms (89.13%) | Memory: 42.9 MB (95.46%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(allCaps(word)|| allLower(word)|| titleCase(word)){
+            return true;
+        }
+        return false;
+    }
+    private boolean allCaps(String word){
+        int count = 0;
+        for(int i =0; i< word.length(); i++){
+            char Ch =word.charAt(i);
+            if(Character.isUpperCase(Ch)){
+                count++;
+            }
+        
+        }
+        if(word.length()== count){
+            return true;
+        }
+        return false;
+    }
+     private boolean allLower(String word){
+        int count = 0;
+        for(int i =0; i<word.length(); i++){
+            char Ch =word.charAt(i);
+            if(Character.isLowerCase(Ch)){
+                count++;
+            }
+        
+        }
+        if(word.length()== count){
+            return true;
+        }
+        return false;
+    }
+    private boolean titleCase(String word){
+        char ch =word.charAt(0);
+        String sub_str = word.substring(1);
+        if(Character.isUpperCase(ch) &&allLower(sub_str)){
+            return true;
+        }
+        return false;
+    }
+}
